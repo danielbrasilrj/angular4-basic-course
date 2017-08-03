a01443d87a27eb8dc9303c9fde0e510563899936
rafael.michelli <rafael.michelli@mprj.mp.br>
2017-08-03 17:07:08 +0000

    Adding ingredients into a Recipe

--- src/app/recipes/recipes.service.ts
+++ src/app/recipes/recipes.service.ts
@@ -1,16 +1,25 @@
 import {EventEmitter} from '@angular/core';
 
 import {Recipe} from './recipe.model';
+import {Ingredient} from "../shared/ingredient.model";
 
 export class RecipesService {
 
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test',
-      'http://www.seriouseats.com/images/2015/09/20150914-pressure-cooker-recipes-roundup-04.jpg'),
-    new Recipe('Another Test Recipe', 'This is another simply a test',
-      'http://www.seriouseats.com/images/2015/09/20150914-pressure-cooker-recipes-roundup-09.jpg')
+    new Recipe('Beans Soup',
+      'A swift soup',
+      'http://www.seriouseats.com/images/2015/09/20150914-pressure-cooker-recipes-roundup-04.jpg',
+      [
+        new Ingredient('Beans', 1), new Ingredient('Carriot', 2), new Ingredient('Meat', 3)
+      ]),
+    new Recipe('Chicken Soup',
+      'A spicy soup',
+      'http://www.seriouseats.com/images/2015/09/20150914-pressure-cooker-recipes-roundup-09.jpg',
+      [
+        new Ingredient('Chicken', 1), new Ingredient('Spice', 3)
+      ])
   ];
 
   getRecipes() {
