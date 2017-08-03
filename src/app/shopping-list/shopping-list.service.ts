33f2450fa27e7e261036196d19479048cc0db9d7
rafael.michelli <rafael.michelli@mprj.mp.br>
2017-08-03 16:46:16 +0000

    using service shopping list to push notifications

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,6 +1,10 @@
 import {Ingredient} from '../shared/ingredient.model';
+import {EventEmitter} from "@angular/core";
 
 export class ShoppingListService {
+
+  ingredientsChanged = new EventEmitter<Ingredient[]>();
+
   private ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
     new Ingredient('Tomatos', 10)
@@ -13,5 +17,6 @@ export class ShoppingListService {
 
   addIngredient(ingredient: Ingredient) {
     this.ingredients.push(ingredient);
+    this.ingredientsChanged.emit(this.ingredients.slice());
   }
 }
