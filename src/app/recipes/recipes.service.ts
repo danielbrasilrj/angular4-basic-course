8979316e3c5da7b9dfb82df9c7d33e1d0d94c95e
rafael.michelli <rafael.michelli@mprj.mp.br>
2017-08-03 14:26:51 +0000

    Creating services

---
+++ src/app/recipes/recipes.service.ts
@@ -0,0 +1,12 @@
+import {Recipe} from './recipe.model';
+
+export class RecipesService {
+
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test',
+      'http://www.seriouseats.com/images/2015/09/20150914-pressure-cooker-recipes-roundup-04.jpg'),
+    new Recipe('Another Test Recipe', 'This is another simply a test',
+      'http://www.seriouseats.com/images/2015/09/20150914-pressure-cooker-recipes-roundup-09.jpg')
+  ];
+
+}
