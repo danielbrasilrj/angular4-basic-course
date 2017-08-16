873ce9c368b01fd2a91192f5e895504da9166a2e
rafael.michelli <rafael.michelli@mprj.mp.br>
2017-08-16 18:35:43 +0000

    Creating a recipe-edit with Reactive Forms (doing ...)

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute, Params} from '@angular/router';
+import {FormArray, FormControl, FormGroup} from '@angular/forms';
+import {RecipesService} from '../recipes.service';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -10,16 +12,55 @@ export class RecipeEditComponent implements OnInit {
 
   id: number;
   editMode = false;
+  recipeForm: FormGroup;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private recipeService: RecipesService) { }
 
   ngOnInit() {
     this.route.params.subscribe(
       (params: Params) => {
         this.id = +params['id'];
         this.editMode = params['id'] != null;
+        this.initForm();
       }
     );
   }
 
+  private initForm() {
+    let recipeId: number;
+    let recipeName = '';
+    let recipeImagePath = '';
+    let recipeDescription = '';
+    let recipeIngredients = new FormArray([]);
+
+    if (this.editMode) {
+      const recipe = this.recipeService.getRecipe(this.id);
+      recipeId = recipe.id;
+      recipeName = recipe.name;
+      recipeImagePath = recipe.imagePath;
+      recipeDescription = recipe.description;
+      if (recipe['ingredients']){
+        for (let ingredient of recipe.ingredients) {
+          recipeIngredients.push(
+            new FormGroup({
+              'name': new FormControl((ingredient.name)),
+              'amount': new FormControl(ingredient.amount)
+            })
+          );
+        }
+      }
+    }
+
+    this.recipeForm = new FormGroup({
+      'id': new FormControl(recipeId),
+      'name': new FormControl(recipeName),
+      'imagePath': new FormControl(recipeImagePath),
+      'description': new FormControl(recipeDescription),
+      'ingredients': recipeIngredients
+    });
+  }
+
+  onSubmit() {
+    console.log(this.recipeForm);
+  }
 }
