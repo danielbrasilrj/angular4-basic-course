f28c79ba152c92ee4a99d3c9912c575f83d0eb1e
rafael.michelli <rafael.michelli@mprj.mp.br>
2017-08-31 18:40:59 +0000

    Including http to get data in firebase

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import {Http, Response} from '@angular/http';
 import {RecipesService} from '../recipes/recipes.service';
 import {Recipe} from '../recipes/recipe.model';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class DataStorageService {
@@ -12,9 +13,21 @@ export class DataStorageService {
   }
 
   getRecipes() {
-    this.http.get('https://ng-recipe-book-3e51b.firebaseio.com/recipes.json').subscribe(
-      (response: Response) => {
-        const recipes: Recipe[] = response.json();
+    this.http.get('https://ng-recipe-book-3e51b.firebaseio.com/recipes.json')
+      // transforming the data to prevent error with ingredients = undefined
+      .map(
+        (response: Response) => {
+          const recipes: Recipe[] = response.json();
+          for (let recipe of recipes) {
+            if (!recipe['ingredients']) {
+              recipe['ingredients'] = [];
+            }
+          }
+          return recipes;
+        }
+      )
+      .subscribe(
+      (recipes: Recipe[]) => {
         this.recipesService.setRecipes(recipes);
       }
     );
