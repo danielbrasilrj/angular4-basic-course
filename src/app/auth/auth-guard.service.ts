28c10562d4fee0de5b8e2723ff63e29a230a4993
rafael.michelli <rafael.michelli@mprj.mp.br>
2017-09-19 13:49:28 +0000

    Implementing a Guard CanActivate in routers.

---
+++ src/app/auth/auth-guard.service.ts
@@ -0,0 +1,15 @@
+import {ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot} from '@angular/router';
+import {Observable} from 'rxjs/Observable';
+import {AuthService} from './auth.service';
+import {Injectable} from '@angular/core';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+
+  constructor(private authService: AuthService) {}
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    return this.authService.isAuthenticated();
+  }
+
+}
